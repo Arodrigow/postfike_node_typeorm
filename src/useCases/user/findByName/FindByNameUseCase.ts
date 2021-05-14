2049ac47b1fb37068330e7de263757e666993788
André Rodrigo B. A. Maciel <rodrigow_@hotmail.com.br>
2021-05-14 11:56:50 -0300

    [Updated] find by name can now find by slice of name

--- src/useCases/user/findByName/FindByNameUseCase.ts
+++ src/useCases/user/findByName/FindByNameUseCase.ts
@@ -6,8 +6,10 @@ import { UserRepository } from "../../../repositories/UserRepository"
 class FindByNameUseCase {
     async execute(user_name: string): Promise<User[]> {
         const userRepository = getCustomRepository(UserRepository);
-        const userFound = await userRepository.find({ name: user_name });
 
+        const userFound = await userRepository.query(
+            'SELECT * FROM users WHERE name LIKE "%' + user_name + '%"'
+        );
         return userFound;
     }
 }
